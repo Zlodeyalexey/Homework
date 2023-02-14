b86a3ad47e7629c4b2d3de208b55d91220a15792
Zlodeyalexey <aluckuts@gmail.com>
2023-02-14 11:59:30 +0300

    homework yeahhh

--- 4.1.py
+++ 4.1.py
@@ -1,9 +1,7 @@
 N = int(input("Сколько надо цифр"))
 M = int(input("Кратные чему"))
 K = int(input("Больше какого числа"))
-x = [i for i in range(0 > K)]
-
+x = [i for i in range(K, N, M)]
 print(x)
-
 
 
