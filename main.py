22c1994df20700bd734c8250d16cdf7ac5a80bfe
Zlodeyalexey <aluckuts@gmail.com>
2023-03-07 09:07:53 +0300

    hw tables

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ conn = connect('db.sqlite3')
 cur = conn.cursor()
 cur: Cursor
 cur.execute('''
-    CREATE TABLE IF NOT EXISTS tablitsa(        
+   CREATE TABLE IF NOT EXISTS category(
          id INTEGER PRIMARY KEY AUTOINCREMENT,
          name VARCHAR(24) NOT NULL UNIQUE
-        
      );
 ''')
+
 conn.commit()
--- products.sql
+++ products.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS products(
          id INTEGER PRIMARY KEY AUTOINCREMENT,
          title VARCHAR(36) NOT NULL UNIQUE,
          description VARCHAR(140) NOT NULL,
-         INTEGER FOREIGN KEY (categories_id) REFERENCES categories (id)
+         category_id INTEGER NOT NULL
+         FOREIGN KEY (category_id) REFERENCES category (id)
      );
