2822aa9bbb56f8f4a022a0896afe7509a77fd671
Stepan Poznyak <boga_pba@mail.ru>
2023-03-07 16:12:53 +0300

    lesson 11

--- models.py
+++ models.py
@@ -5,9 +5,30 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr
 class Base(DeclarativeBase):
     pk = Column('id', INT, primary_key=True)
 
+cat = Category(name='cofee')
+
+
     @declared_attr
     def __tablename__(cls):
         return ''.join(f'_{i.lower()}' if i.isupper() else i for i in cls.__name__).strip('_')
+    @staticmethod
+    def create_session(func):
+        def wrapper(*args, **kwargs):
+            with Base.session() as session:
+                return func(*args, **kwargs, session=session)
+
+    return wrapper
+
+    @create_session
+    def save(self, session: Session = None)-> None:
+        session.add(self)
+        session.commit()
+        session.refresh(self)
+
+    @classmethod
+    @create_session
+    def get(cls, pk, session: Session = None):
+        return session.get(cls, pk)
 
 
 class Category(Base):
--- test.py
+++ test.py
@@ -6,9 +6,22 @@ from models import Category
 
 engine = create_engine('postgresql://login:password@localhost:5432/data')
 session = sessionmaker(bind=engine)
+# with session() as s:
+#     cat = Category(name='Beer')
+#     s.add(cat)
+#     s.commit()
+#     s.refresh(cat)
+#     print(cat.pk, cat.name)
+
 with session() as s:
-    cat = Category(name='Beer')
-    s.add(cat)
-    s.commit()
-    s.refresh(cat)
-    print(cat.pk, cat.name)
+#     cat = s.get(Category, 1)
+#     print(cat.name)
+    objs = s.scalars(
+        select(Category)
+        .order_by(Category.name.desc())
+        .limit(1)
+        .offset(2)
+        .filter()
+        .where()
+        .filter_by()
+    )
