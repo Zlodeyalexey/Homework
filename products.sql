5e956b662366f58072eacfedbb81d1dba6e25e71
Zlodeyalexey <aluckuts@gmail.com>
2023-03-07 09:28:32 +0300

    hw tables

--- products.sql
+++ products.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS products(
          id INTEGER PRIMARY KEY AUTOINCREMENT,
          title VARCHAR(36) NOT NULL UNIQUE,
          description VARCHAR(140) NOT NULL,
-         category_id INTEGER NOT NULL
+         category_id INTEGER NOT NULL,
          FOREIGN KEY (category_id) REFERENCES category (id)
      );
