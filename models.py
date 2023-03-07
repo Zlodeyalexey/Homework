1128fe2e67d360f10836d781611fce39638325b6
Stepan Poznyak <boga_pba@mail.ru>
2023-03-07 16:34:18 +0300

    lesson 11

--- models.py
+++ models.py
@@ -30,9 +30,32 @@ cat = Category(name='cofee')
     def get(cls, pk, session: Session = None):
         return session.get(cls, pk)
 
+    @create_session
+    def delete(self, session: Session =None):
+        session.delete(self)
+        session.commit()
+
+    @classmethod
+    @create_session
+    def scalars(cls, sql, session: Session = None):
+        return session.scalars(sql).all()
+
+    @classmethod
+    @create-session
+    def execute(cls, sql, session: Session = None):
+        return session.execute(sql).all()
+
+    def dict(self):
+        data = self.__dict__
+        data['id'] = data['pk']
+        del data['pk']
+        if '_sa_instance_state' in data:
+            del data['_sa_instance_state']
+        return data
+
 
 class Category(Base):
-    name = Column(VARCHAR(64), nullable=False, unique=True)
+name = Column(VARCHAR(64), nullable=False, unique=True)
 
 
 class Product(Base):
@@ -41,6 +64,11 @@ class Product(Base):
     descr = Column(VARCHAR(4096), nullable=True)
     is_published = Column(BOOLEAN, default=False)
     category_id = Column(INT, ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
+
+    @property
+    def category(self):
+        with self.session() as session:
+            return session.get(Category, self.category_id)
 
 
 
