ce8cbb1bcb192434fe49f80e6a7adf12b47ec931
Zlodeyalexey <aluckuts@gmail.com>
2023-03-13 11:57:28 +0300

    hw tables

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from sqlite3 import connect, Cursor
+from sqlalchemy import create_engine, select, or_, and_, any_, all_
+from sqlalchemy.orm import sessionmaker
+
+from models import Category
 
 
-conn = connect('db.sqlite3')
-cur = conn.cursor()
-cur: Cursor
-cur.execute('''
-   CREATE TABLE IF NOT EXISTS category(
-         id INTEGER PRIMARY KEY AUTOINCREMENT,
-         name VARCHAR(24) NOT NULL UNIQUE
-     );
-''')
+engine = create_engine('postgresql://student:password@localhost:5432/homework')
+session = sessionmaker(bind=engine)
+# with session() as s:
+#     cat = Category(name ='Beer')
+#     s.add(cat)
+#     s.commit()
+#     s.refresh(cat)
+#     print(cat.pk, cat.name)
 
-conn.commit()
+with session() as s:
+    # cat =  s.get(Category, 1)
+    # print(cat.name)
+    objs = s.scalars (
+        select(Category)
+        .order_by(Category.name.desc())
+        # .filter(
+        #     or_(Category.pk > 0, Category.name.like('%ee%'))
+        # )
+        # .filter_by()
+        # .limit(1)
+        # # .offset(2)
+    )
+    print(objs.all())
